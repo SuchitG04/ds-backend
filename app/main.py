476663c5520bfe19fc51d9bd15c43166f798fbb3
Suchit G <suchitg04@gmail.com>
2025-01-30 22:46:58 +0530

    ref: allow all origins

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ app.include_router(quizzes_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
