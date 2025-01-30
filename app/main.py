3b7f3028415e937c2e20eca5a24b0bea1f77710b
uttamkn <uttamkn15@gmail.com>
2025-01-30 14:46:27 +0530

    fix: add cors middleware

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from database import create_tables, conn
 
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -17,6 +18,13 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(user_router)
 app.include_router(quizzes_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
