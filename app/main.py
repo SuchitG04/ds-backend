0153bd960478eefaee99ddd1d0e061838889d88c
Suchit G <suchitg04@gmail.com>
2025-01-30 12:43:50 +0530

    fix: add updated main.py with new routers

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-from fastapi import FastAPI
+from user import router as user_router
+from database import create_tables, conn
 
-app = FastAPI()
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_tables()
+    yield
+    conn.close()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(user_router)
 
 @app.get("/")
 def read_root():
