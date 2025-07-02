431b0e7560c368d4f0866859ce983679e2d6d698
Suchit G <suchitg04@gmail.com>
2025-07-02 17:00:24 +0530

    use uvicorn; remove rel imps

--- app/quizzes.py
+++ app/quizzes.py
@@ -1,7 +1,7 @@
 from sqlite3 import DatabaseError
 from fastapi import APIRouter, HTTPException
 import logging
-from .database import get_db_connection
+from app.database import get_db_connection
 
 router = APIRouter()
 
--- container_startup.sh
+++ container_startup.sh
@@ -4,4 +4,4 @@ python populate_queue_quizzes.py
 python populate_recursion_quizzes.py
 python populate_videos.py
 
-fastapi run app/main.py --port 8001
+uvicorn app.main:app --host 0.0.0.0 --port 8001
